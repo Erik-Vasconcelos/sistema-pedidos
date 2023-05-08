e470c4310746de035bd5cce063a21f481fcabae6
Erik-Vasconcelos <erikvasconcelos2018@gmail.com>
2023-05-08 16:21:16 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/nelioalves/cursomc/sp/SistemaPedidoApplication.java
+++ src/main/java/com/nelioalves/cursomc/sp/SistemaPedidoApplication.java
@@ -7,12 +7,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nelioalves.cursomc.sp.domain.Categoria;
 import com.nelioalves.cursomc.sp.domain.Cidade;
+import com.nelioalves.cursomc.sp.domain.Cliente;
+import com.nelioalves.cursomc.sp.domain.Endereco;
 import com.nelioalves.cursomc.sp.domain.Estado;
-import com.nelioalves.cursomc.sp.domain.Produto;
+import com.nelioalves.cursomc.sp.domain.enums.TipoCliente;
 import com.nelioalves.cursomc.sp.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.sp.repositories.CidadeRepository;
+import com.nelioalves.cursomc.sp.repositories.ClienteRepository;
+import com.nelioalves.cursomc.sp.repositories.EnderecoRepository;
 import com.nelioalves.cursomc.sp.repositories.EstadoRepository;
 import com.nelioalves.cursomc.sp.repositories.ProdutoRepository;
 
@@ -27,6 +30,10 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidoApplication.class, args);
@@ -45,7 +52,7 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));   
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
@@ -62,8 +69,19 @@ public class SistemaPedidoApplication implements CommandLineRunner {
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
-		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));*/
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		Cliente cli1 = new Cliente("Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco("Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco("Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		*/
 		
 	}
 
---
+++ src/main/java/com/nelioalves/cursomc/sp/domain/enums/TipoCliente.java
@@ -0,0 +1,45 @@
+package com.nelioalves.cursomc.sp.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+
+	private int codigo;
+	private String descricao;
+
+	private TipoCliente(int codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public static TipoCliente toEnum(Integer codigo) {
+		if (codigo == null)
+			return null;
+
+		for (TipoCliente tipo : TipoCliente.values()) {
+			if (codigo.equals(tipo.getCodigo())) {
+				return tipo;
+			}
+		}
+
+		throw new IllegalArgumentException("Código inválido: " + codigo);
+	}
+
+}
